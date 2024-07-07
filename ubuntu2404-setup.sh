4c67055de2ffcb4626b8fb0edd9fd6e9390ff5ff
bruno@loka <bruno@loka.com>
2024-07-07 13:16:33 +0100

    moved Brave to snap

---
+++ ubuntu2404-setup.sh
@@ -0,0 +1,11 @@
+# apt
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y \
+  byobu curl feh ffmpeg git \
+  ibus-mozc imagemagick jq \
+  network-manager-l2tp network-manager-l2tp-gnome \
+  pinentry-tty pkg-config \
+  rhythmbox unar w3m wget zsh
+
+  
