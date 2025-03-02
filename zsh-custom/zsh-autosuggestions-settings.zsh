1eda54702c0d8836709705c0e72b20afb76e1322
bruno@loka <bruno@loka.com>
2025-03-02 18:40:46 +0000

    fix zsh-autosuggestion bind

--- zsh-custom/zsh-autosuggestions-settings.zsh
+++ zsh-custom/zsh-autosuggestions-settings.zsh
@@ -1,6 +1,6 @@
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
 # Bind 'W' (uppercase) to accept the autosuggestion
-bindkey 'W' autosuggest-accept
+bindkey '#' autosuggest-accept
 
 # Bind 'Ctrl + W' to execute the autosuggestion
 bindkey '^W' autosuggest-execute
