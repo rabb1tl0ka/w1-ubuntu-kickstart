d12d33372e68d55d60e0f2217222305f41277f43
bruno@loka <bruno@loka.com>
2025-09-06 18:40:45 +0100

    install gnome-shell and vimix theme

---
+++ gnome-shell/gnome-shell-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y gnome-tweaks gtk2-engines-murrine gtk2-engines-pixbuf
---
+++ gnome-shell/install-vimix-gtk-themes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/vinceliuice/Vimix-gtk-themes.git
+cd Vimix-gtk-themes
+./install.sh -t all -s all --libadwaita
